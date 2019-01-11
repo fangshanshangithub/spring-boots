d6955b064a846c5649f53e6f6acdee21ef97da94
fangshanshan <643915247@qq.com>
2019-01-11 18:04:21 +0800

    1.11 公司提交

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -1,5 +1,5 @@
 drop database  if exists  spring_boots_db;
-create database spring_boots_db;
+create database spring_boots_db CHARACTER SET utf8 COLLATE utf8_general_ci;;
 use spring_boots_db;
 
 drop table if exists user_info;
@@ -10,4 +10,13 @@ create table user_info(
   email varchar(20) not null default '' comment '用户邮箱',
   phone varchar(20) not null default '' comment '用户手机号'
 
-)
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+drop table if exists student_source;
+create table student_source(
+  id bigint(10) not null auto_increment primary key comment '主键',
+  name varchar(18) not null comment '学生姓名',
+  object varchar(10) not null comment '学科',
+  source int(10) not null  comment '分数'
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
---
+++ sql/initTable.sql
@@ -0,0 +1,17 @@
+insert into student_source values (null, '张三', '数学',80);
+insert into student_source values (null, '李四', '数学',23);
+insert into student_source values (null, '王五', '数学',35);
+insert into student_source values (null, '赵六', '数学',78);
+insert into student_source values (null, '张钱', '数学',88);
+
+insert into student_source values (null, '张三', '语文',70);
+insert into student_source values (null, '李四', '语文',58);
+insert into student_source values (null, '王五', '语文',98);
+insert into student_source values (null, '赵六', '语文',78);
+insert into student_source values (null, '张钱', '语文',99);
+
+insert into student_source values (null, '张三', '英语',70);
+insert into student_source values (null, '李四', '英语',85);
+insert into student_source values (null, '王五', '英语',88);
+insert into student_source values (null, '赵六', '英语',98);
+insert into student_source values (null, '张钱', '英语',99);
