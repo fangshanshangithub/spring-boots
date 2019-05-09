10d5d4fd13831925c1e5149451f4c0c6c4de6d21
FangShanShanFamily <643915247@qq.com>
2019-05-09 23:28:28 +0800

    spring-boot-base 项目

---
+++ spring-boots-base/src/main/java/com/boots/base/SpringBootsBaseApplication.java
@@ -0,0 +1,13 @@
+package com.boots.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootsBaseApplication.class, args);
+    }
+
+}
