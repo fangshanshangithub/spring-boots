7812230ed4ef0b8b8afb4f72ab0af605e9f13ebc
FangShanShanFamily <643915247@qq.com>
2019-05-20 00:40:51 +0800

    代码同步-- 家里

--- spring-boots-redis/src/main/java/com/boot/redis/dao/UserInfoMapper.java
+++ spring-boots-redis/src/main/java/com/boot/redis/dao/UserInfoMapper.java
@@ -1,6 +1,7 @@
 package com.boot.redis.dao;
 
 import com.boot.redis.entity.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
 
 public interface UserInfoMapper {
     int deleteByPrimaryKey(Long id);
--- spring-boots-redis/src/test/java/com/boot/redis/SpringBootsRedisApplicationTests.java
+++ spring-boots-redis/src/test/java/com/boot/redis/SpringBootsRedisApplicationTests.java
@@ -1,7 +1,9 @@
 package com.boot.redis;
 
+import com.boot.redis.utils.RedisUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootsRedisApplicationTests {
 
+	@Autowired
+	private RedisUtils redisUtils;
+
 	@Test
 	public void contextLoads() {
+		redisUtils.set("aa",122222);
 	}
 
 }
