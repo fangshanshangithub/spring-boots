822e2cda81a3f8ce4a7fd67de2a0572ed1cfe50c
fangshanshan@lexue.com <fangshanshan@lexue.com>
2019-05-22 20:50:56 +0800

    公司提交

--- spring-boots-redis/src/main/java/com/boot/redis/controller/RedisController.java
+++ spring-boots-redis/src/main/java/com/boot/redis/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.boot.redis.controller;
 
+import com.fangss.utils.redisutils.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
